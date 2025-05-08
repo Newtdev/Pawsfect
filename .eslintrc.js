d1f9fdefa2496d7d27520f8aef64880410354dcf
Thomas Ejembi <ejembithomas61@gmail.com>
2025-05-08 11:23:28 +0100

    Chore:.eslint.js rules, plugins and completed setup done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,24 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    'eslint:recommeded',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@react-native',
+  ],
+  parser: ['@typescript-eslint', 'react'],
+  plugins: ['typescript', 'react', 'react-native', 'jest'],
+  env: {
+    node: true,
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-unsued-vars': 'warn',
+    'react/props-types': 'off',
+    'react-native/no-inline-styles': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-module-boundary-type': 'error',
+    '@typescript-eslint/func-call-spacing': 'off',
+    '@typescript-eslint/no-require-imports': 'warn',
+  },
 };
