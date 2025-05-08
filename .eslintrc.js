ac05752349124a89265595d66fffc36e8aa2fec5
Thomas Ejembi <ejembithomas61@gmail.com>
2025-05-08 15:10:16 +0100

    Fix: update typescript-eslint to typescript-eslint/parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,22 @@
 module.exports = {
   root: true,
   extends: [
-    'eslint:recommeded',
+    'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     '@react-native',
   ],
-  parser: ['@typescript-eslint', 'react'],
-  plugins: ['typescript', 'react', 'react-native', 'jest'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-native', 'jest'],
   env: {
     node: true,
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'no-unsued-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
     'react/props-types': 'off',
     'react-native/no-inline-styles': 'error',
     '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/explicit-module-boundary-type': 'error',
     '@typescript-eslint/func-call-spacing': 'off',
     '@typescript-eslint/no-require-imports': 'warn',
   },
--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,10 @@ module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
+  jsxBracketSameline: true,
+  jsxSingleQuote: true,
   singleQuote: true,
   trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
 };
