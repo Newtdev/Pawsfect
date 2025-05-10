0c87ba0ee10e40f2ac672e7d68b82e8830b5a35d
Thomas Ejembi <ejembithomas61@gmail.com>
2025-05-10 20:14:09 +0100

    chore:custom font setup and reorganization of the folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
-import SVGWrapper from './src/shared/components/SVGWrapper';
+import SVGWrapper from './shared/components/SVGWrapper';
 
 function App(): React.JSX.Element {
   return (
     <View>
-      <SVGWrapper name="logo" />
-      <Text>Pawfect</Text>
+      <SVGWrapper name="logo" height={36} width={36} fill="red" />
+      <Text>Great Shopping Experience</Text>
     </View>
   );
 }
